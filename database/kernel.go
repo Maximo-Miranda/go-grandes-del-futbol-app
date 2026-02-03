0cdc2fded2e3f15d229e38d005f024b1cca094b5
Maximo-Miranda <maximomirandah@gmail.com>
2026-02-02 23:20:34 -0500

    fix: register all 13 migrations in database kernel

--- database/kernel.go
+++ database/kernel.go
@@ -3,12 +3,28 @@ package database
 import (
 	"github.com/goravel/framework/contracts/database/schema"
 	"github.com/goravel/framework/contracts/database/seeder"
+
+	"grandesdelfutbol/database/migrations"
 )
 
 type Kernel struct{}
 
 func (kernel Kernel) Migrations() []schema.Migration {
-	return []schema.Migration{}
+	return []schema.Migration{
+		&migrations.M20260201000001CreateUsersTable{},
+		&migrations.M20260201000002CreatePlayersTable{},
+		&migrations.M20260201000003CreateVenuesTable{},
+		&migrations.M20260201000004CreateTournamentsTable{},
+		&migrations.M20260201000005CreateTeamsTable{},
+		&migrations.M20260201000006CreateGroupsTable{},
+		&migrations.M20260201000007CreateTournamentTeamsTable{},
+		&migrations.M20260201000008CreateTeamPlayersTable{},
+		&migrations.M20260201000009CreateMatchesTable{},
+		&migrations.M20260201000010CreateMatchEventsTable{},
+		&migrations.M20260201000011CreateMatchLineupsTable{},
+		&migrations.M20260201000012CreateStandingsTable{},
+		&migrations.M20260201000013CreateJobsTable{},
+	}
 }
 
 func (kernel Kernel) Seeders() []seeder.Seeder {
